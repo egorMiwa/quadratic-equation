6ae96b2fd273f1093cd7f6b15d330e5f52154fe8
egorMiwa <36693566+egorMiwa@users.noreply.github.com>
2018-02-26 18:24:44 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  var arr = equation.split(' '),
+      first  = parseFloat(arr[0]),
+      second   = parseFloat(arr[arr.lastIndexOf('*') - 2] + arr[arr.lastIndexOf('*') - 1]),
+      third   = parseFloat(arr[arr.length - 2] + arr[arr.length - 1]),
+
+      D   = Math.pow(second, 2) - 4 * first * third,
+      x1  = Math.round((-second - Math.sqrt(D)) / (2 * first)),
+      x2  = Math.round((-second + Math.sqrt(D)) / (2 * first));
+
+  if(x1 > x2) {
+    return [x2, x1];
+  }
+    return [x1,x2];
+ }
